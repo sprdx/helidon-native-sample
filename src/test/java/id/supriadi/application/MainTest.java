03575943156c0027246aaaa7f6a37135e4d2c663
sprdx <supriadi15001@gmail.com>
2024-08-10 11:40:26 +0700

    second

---
+++ src/test/java/id/supriadi/application/MainTest.java
@@ -0,0 +1,11 @@
+package id.supriadi.application;
+
+import io.helidon.webserver.testing.junit5.DirectClient;
+import io.helidon.webserver.testing.junit5.RoutingTest;
+
+@RoutingTest
+class MainTest extends AbstractMainTest {
+    MainTest(DirectClient client) {
+        super(client);
+    }
+}
